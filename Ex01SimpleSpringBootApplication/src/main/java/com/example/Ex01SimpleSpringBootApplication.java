e40eba8d2d9904dc5eeb6c82b20553dcecf32f96
partha biswas <biswas.partha1@gmail.com>
2024-04-10 03:57:09 +0530

    simple spring boot application

---
+++ Ex01SimpleSpringBootApplication/src/main/java/com/example/Ex01SimpleSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Ex01SimpleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.example.Ex01SimpleSpringBootApplication.class, args);
+	}
+
+}
---
+++ Ex01SimpleSpringBootApplication/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,47 @@
+package com.example.entity;
+
+import java.util.List;
+
+
+
+import com.example.request.CreateStudentRequest;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@OneToOne
+	@JoinColumn(name = "address_id")
+	private Address address;
+	
+	@OneToMany(mappedBy = "student")
+	private List<Subject> learningSubjects;
+	
+	public Student (CreateStudentRequest createStudentRequest) {
+		this.firstName = createStudentRequest.getFirstName();
+		this.lastName = createStudentRequest.getLastName();
+		this.email = createStudentRequest.getEmail();
+	}
+}
