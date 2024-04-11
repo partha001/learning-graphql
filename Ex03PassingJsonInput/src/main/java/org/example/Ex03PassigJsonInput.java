98c7083a22f3efbd0da724cff90a268cef808185
partha biswas <biswas.partha1@gmail.com>
2024-04-11 23:37:17 +0530

    passing json as request parameter

---
+++ Ex03PassingJsonInput/src/main/java/org/example/Ex03PassigJsonInput.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03PassigJsonInput {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03PassigJsonInput.class, args);
+	}
+
+}
