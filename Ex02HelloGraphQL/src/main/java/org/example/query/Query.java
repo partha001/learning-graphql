91707315d46a9bb82b71206c15ab4d0aa653a016
partha biswas <biswas.partha1@gmail.com>
2024-04-11 11:34:44 +0530

    adding parameterized endpoint to the application

--- Ex02HelloGraphQL/src/main/java/org/example/query/Query.java
+++ Ex02HelloGraphQL/src/main/java/org/example/query/Query.java
@@ -1,5 +1,6 @@
 package org.example.query;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -14,5 +15,10 @@ public class Query {
     @QueryMapping
     public String secondQuery(){
         return "SecondQuery";
+    }
+
+    @QueryMapping
+    public String fullName(@Argument String firstname,@Argument String lastname){
+        return firstname + " "+ lastname;
     }
 }
