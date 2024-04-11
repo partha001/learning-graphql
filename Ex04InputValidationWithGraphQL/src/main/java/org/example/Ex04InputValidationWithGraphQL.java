1a442614714bdbb09878043e04ccab2fb2b16186
partha biswas <biswas.partha1@gmail.com>
2024-04-11 23:55:18 +0530

    adding input validation in graphql

---
+++ Ex04InputValidationWithGraphQL/src/main/java/org/example/Ex04InputValidationWithGraphQL.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex04InputValidationWithGraphQL {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex04InputValidationWithGraphQL.class, args);
+	}
+
+}
